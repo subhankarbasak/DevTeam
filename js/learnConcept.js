a78c03dd1d8d6524c4744246732c36fd304bc451
Subhankar <subhankarbasakmail@gmail.com>
2023-02-03 21:57:27 +0530

    Day 3 - Call, Apply and Bind Method

---
+++ js/learnConcept.js
@@ -0,0 +1,75 @@
+
+// #########################
+// Call Method
+//############################
+
+// function getEmail(xy,z){
+//     console.log(xy,z)
+//         return this.firstName +"."+this.lastName+"@test.com";
+//     }
+
+// let student={
+//     "firstName":"sibu",
+//     "lastName":"basak",
+//     "age":24,
+// }
+
+// let teacher={
+//     "firstName":"Subhankar",
+//     "lastName":"basak",
+//     "age":24,
+// }
+
+// console.log(getEmail.call(student,"hello","bye"));
+
+
+// #########################
+// Apply Method
+//############################
+
+// function getEmail(xy){
+//     console.log(xy)
+//         return this.firstName +"."+this.lastName+"@test.com";
+//     }
+
+// let student={
+//     "firstName":"sibu",
+//     "lastName":"basak",
+//     "age":24,
+// }
+
+// let teacher={
+//     "firstName":"Subhankar",
+//     "lastName":"basak",
+//     "age":24,
+// }
+
+// console.log(getEmail.apply(student,["hello","bye"]));
+
+
+
+// #########################
+// Bind Method
+//############################
+
+
+//bind method
+
+function getEmail() {
+    return this.firstName + "." + this.lastName + "@test.com";
+}
+
+let student = {
+    "firstName": "sibu",
+    "lastName": "basak",
+    "age": 24,
+
+}
+
+let teacher = {
+    "firstName": "Subhankar",
+    "lastName": "basak",
+    "age": 24,
+}
+let email = getEmail.bind(student);
+console.log(email());
